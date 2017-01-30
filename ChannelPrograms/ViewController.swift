da9d3b74dd361093492fe40cc4efe9c6cb541105
KolyaKolya95 <kolya@macbook-pro-kolya.local>
2017-01-30 16:40:11 +0200

    first commit

--- ChannelPrograms/ViewController.swift
+++ ChannelPrograms/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("Перший коміт")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
