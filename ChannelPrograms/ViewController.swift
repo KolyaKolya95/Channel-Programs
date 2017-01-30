fe2b29f0a58e352c0bbfc8aa68b6dd84a3e693a8
KolyaKolya95 <kolya@macbook-pro-kolya.local>
2017-01-30 16:23:45 +0200

    Initial Commit

---
+++ ChannelPrograms/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChannelPrograms
+//
+//  Created by Kolya on 30.01.17.
+//  Copyright © 2017 Kolya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
